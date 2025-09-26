b9957c21bd66b40ad434196baa69959ab0530ffd
ashardevops5-wq <ashar.devops5@gmail.com>
2025-09-26 14:17:04 +0500

    Update hello.js

--- hello.js
+++ hello.js
@@ -4,10 +4,3 @@ console.log("Integration successful!");
 console.log("Goodbye Jenkins!");
 console.log("Final test message.");
 console.log("End of script.");
-
-// Import Student class
-const Student = require('./Student');
-
-// Create object and call method
-const s1 = new Student();
-s1.study();
