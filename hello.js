d05a883dc7b92cfcef07c990cd5bfb9896b78526
ashar <ashar.devops5@gmail.com>
2025-09-26 13:28:43 +0500

    this is update file tester

--- hello.js
+++ hello.js
@@ -1,6 +1,13 @@
-// hello.js
 console.log("Hello Jenkins!");
 console.log("Testing GitHub to Jenkins integration...");
 console.log("Integration successful!");
 console.log("Goodbye Jenkins!");
 console.log("Final test message.");
+console.log("End of script.");
+
+// Import Student class
+const Student = require('./Student');
+
+// Create object and call method
+const s1 = new Student();
+s1.study();
--- student.js
+++ student.js
@@ -1,10 +1,11 @@
 class Student {
-
-    constructor(){
+    constructor() {
         console.log("Student class constructor called.");
     }
-    study(){
+    study() {
         console.log("Student is studying.");
+        console.log("Student is learning new things.");
     }
-
 }
+
+module.exports = Student;
