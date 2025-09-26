ead5504b947a2db447f13b945edfdd833ed0893a
ashar <ashar.devops5@gmail.com>
2025-09-26 12:50:17 +0500

    2 pipiline tester commit

--- hello.js
+++ hello.js
@@ -1,3 +1,6 @@
 // hello.js
 console.log("Hello Jenkins!");
 console.log("Testing GitHub to Jenkins integration...");
+console.log("Integration successful!");
+console.log("Goodbye Jenkins!");
+console.log("Final test message.");
